8379b96525e840156f394afd25ceb6b562a68412
Burimov <denysburimov@gmail.com>
2018-01-12 17:55:20 +0200

    работает метод определения повторения

--- Ukr073.py
+++ Ukr073.py
@@ -1,14 +1,21 @@
 class SuperStr(str):
-    a = 1
-    b = 2
+    row = ''
 
-    def is_repeatance(self):
-        pass
+    def __init__(self, row):
+        self.row = row
+
+    def is_repeatance(self, s):
+        len_p = len(self.row)/len(s)
+        if s*len_p == self.row:
+            return True
+        else:
+            return False
+
 
     def is_palindrom(self):
         pass
 
 
-unit = SuperStr()
+s = SuperStr('123123123123')
 
-print unit.a
+print s.is_repeatance('123')
