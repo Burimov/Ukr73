ec61c07a2c71871ab1c52f795c2d45426071ac8d
Burimov <denysburimov@gmail.com>
2018-01-12 18:14:16 +0200

    набрана структура полиндрома

--- Ukr073.py
+++ Ukr073.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 class SuperStr(str):
     row = ''
 
@@ -5,17 +7,32 @@ class SuperStr(str):
         self.row = row
 
     def is_repeatance(self, s):
-        len_p = len(self.row)/len(s)
-        if s*len_p == self.row:
+        len_p = len(self.row) / len(s)
+        if s * len_p == self.row:
             return True
         else:
             return False
 
-
     def is_palindrom(self):
-        pass
+        # -*- coding: utf-8 -*-
+
+        chet = len(self)%2
+        half_len = len(self)/2
+        l_begin = []
+        l_end = []
+
+        # l_begin набить прямой строкой
+        # l_end набить реверснутой
+        # сравнить, одинаковы ли они до половины
 
 
-s = SuperStr('123123123123')
+        for i in range(half_len):
+            pass
 
-print s.is_repeatance('123')
+        return self
+
+
+s = SuperStr('121121121121')
+
+print s.is_repeatance('121')
+print s.is_palindrom()
